8752461164a61d301a775516c57cfa42ebc50b74
Tyler Johnston <tylergarrettjohnston@gmail.com>
2024-06-02 04:36:17 -0400

    Get project running and connected to database

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+app.use(cors());
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
+const mongoose = require("mongoose");
 
-
-
+mongoose.connect(process.env.MONGO_URI);
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log("Your app is listening on port " + listener.address().port);
+});
